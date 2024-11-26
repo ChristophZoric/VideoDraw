d5f70ee9068891c473c555ac3138c5fb8e52639f
Rafael Canete <rafael.canete2004@gmail.com>
2024-11-26 14:13:33 +0100

    multithreading skizze

--- classification/test.py
+++ classification/test.py
@@ -1,14 +1,17 @@
 import numpy as np
 from tensorflow.keras.models import load_model
 
+
 class Predictor:
     @staticmethod
     def load_and_test_coordinates(np_file_path, model_path='classification/saved_model/coordinate_model.h5'):
+        print("entered Predictor")
         model = load_model(model_path)
 
         # Koordinatenbild laden und in das Modell-Format umwandeln
         class_data = np.load(np_file_path)
-        coordinates = class_data.reshape((1, 36, 36, 1))  # Anpassung an 64x64 Pixel
+        coordinates = class_data.reshape(
+            (1, 36, 36, 1))  # Anpassung an 64x64 Pixel
 
         # Vorhersage
         prediction = model.predict(coordinates)
