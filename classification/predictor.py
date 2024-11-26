74d37eea90fc8b6da5f91942e15c85c6f6a08f47
Rafael Canete <rafael.canete2004@gmail.com>
2024-11-26 15:46:25 +0100

    asynchronous workflow established with classification entrypoint

---
+++ classification/predictor.py
@@ -0,0 +1,21 @@
+import numpy as np
+from tensorflow.keras.models import load_model
+
+
+class Predictor:
+    def __init__(self, model_path='classification/saved_model/coordinate_model.h5'):
+        self.model = load_model(model_path)
+        self.class_names = ['basketball', 'car', 'plane']
+
+    def predict(self, np_file_path):
+        print("entered Predictor")
+
+        # Koordinatenbild laden und in das Modell-Format umwandeln
+        data = np.load(np_file_path)
+        coordinates = data.reshape(
+            (1, 36, 36, 1))  # Anpassung an 64x64 Pixel
+
+        # Vorhersage
+        predictions = self.model.predict(coordinates)
+        predicted_class = np.argmax(predictions[0])
+        return self.class_names[predicted_class]
