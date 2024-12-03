4c84a61e4c7d43358750aed83c99c68b54e2e687
RafaelCanete <rafael.canete2004@gmail.com>
2024-12-03 14:56:45 +0100

    prettier hand gesture display

--- interface/app.py
+++ interface/app.py
@@ -416,19 +416,19 @@ def draw_bounding_rect(use_brect, image, brect):
 
 def draw_info_text(image, brect, handedness, hand_sign_text,
                    finger_gesture_text):
-    cv.rectangle(image, (brect[0], brect[1]), (brect[2], brect[1] - 22),
-                 (0, 0, 0), -1)
+    #    cv.rectangle(image, (brect[0], brect[1]), (brect[2], brect[1] - 22),
+    # (0, 0, 0), -1)
 
     info_text = handedness.classification[0].label[0:]
     if hand_sign_text != "":
         info_text = info_text + ':' + hand_sign_text
-    cv.putText(image, info_text, (brect[0] + 5, brect[1] - 4),
-               cv.FONT_HERSHEY_SIMPLEX, 0.6, (255, 255, 255), 1, cv.LINE_AA)
+#    cv.putText(image, info_text, (brect[0] + 5, brect[1] - 4),
+        # cv.FONT_HERSHEY_SIMPLEX, 0.6, (255, 255, 255), 1, cv.LINE_AA)
 
     if finger_gesture_text != "":
-        cv.putText(image, "Finger Gesture:" + finger_gesture_text, (10, 100),
+        cv.putText(image, hand_sign_text, (brect[0] + 30, brect[1] - 30),
                    cv.FONT_HERSHEY_SIMPLEX, 1.0, (0, 0, 0), 4, cv.LINE_AA)
-        cv.putText(image, "Finger Gesture:" + finger_gesture_text, (10, 100),
+        cv.putText(image, hand_sign_text,  (brect[0] + 30, brect[1] - 30),
                    cv.FONT_HERSHEY_SIMPLEX, 1.0, (255, 255, 255), 2,
                    cv.LINE_AA)
 
